34e209c6804b99041c40c796bb5c1e716fec6cef
ValentynKurylo <56786816+ValentynKurylo@users.noreply.github.com>
2023-02-22 19:01:38 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {useSelector} from "react-redux";
+
 import './App.css';
+import Navbar from "./components/navbar/navbar"
+import Login from "./components/auth/login";
+import Registration from "./components/auth/registration";
+import Task from "./components/task/task"
 
 function App() {
+    const UserReducer = useSelector((state => state.UserReducer))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+            <Navbar/>
+          {!UserReducer.isAuth ?
+              <Switch>
+                  <Route exact={true} path={'/'} component={Login}/>
+                  <Route exact={true} path={'/login'} component={Login}/>
+                  <Route exact={true} path={'/registration'} component={Registration}/>
+              </Switch> :
+              <Switch>
+                  <Route exact={true} path={''} component={Task}/>
+              </Switch>
+          }
+
+      </BrowserRouter>
     </div>
   );
 }
